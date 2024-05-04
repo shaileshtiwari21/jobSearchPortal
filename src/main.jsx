84dfe3fed5bf76cbd21a9d66b56963d27111f98a
shaileshtiwari21 <saffron.shaileshtiwari@gmail.com>
2024-05-04 21:04:09 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import JobSearch from "../src/pages/jobSearch";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <JobSearch />
+  </React.StrictMode>
+);
---
+++ src/pages/jobSearch.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const jobSearch = () => {
+  return <div>jobSearch</div>;
+};
+
+export default jobSearch;
